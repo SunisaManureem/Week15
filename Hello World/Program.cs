abb6e65406547836578149d1ba6c8b14a063952a
SunisaManureem <sunisa2561a@gmail.com>
2024-10-18 11:42:45 +0700

    first commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Hello_World;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        c = Add(a, b);
+        Console.WriteLine($"The sum of {a} and {b} is {c}");
+        
+    }
+
+    static int Add(int var1, int var2)
+    {
+        return var1 + var2;
+    }
+}
+
